a33536d4d1368418501326b200f87cf7cc5e209d
Vlad Usenko <37244741+vladdusenko@users.noreply.github.com>
2020-10-27 00:13:54 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,4 @@
 
- * @version 2.1.6
- * @author Bartosz Wojciechowski
- * @author David Deutsch
- * @license The MIT License (MIT)
- * @todo Lazy Load Icon
- * @todo prevent animationend bubling
- * @todo itemsScaleUp
- * @todo Test Zepto
- * @todo stagePadding calculate wrong active classes
- */
 ;(function($, window, document, undefined) {
 
 	/**
